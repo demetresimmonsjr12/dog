77351d31ed5051d89f1080c1c9e142fcdb0c7c83
demetresimmonsjr12 <135878578+demetresimmonsjr12@users.noreply.github.com>
2023-06-09 15:18:01 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.InBackground)
+    if (0 == receivedNumber) {
+        basic.showIcon(IconNames.Heart)
+        xgo.execution_action(xgo.action_enum.Sit_down)
+    } else if (1 == receivedNumber) {
+        basic.showIcon(IconNames.Silly)
+        xgo.move_xgo(xgo.direction_enum.Forward, 65)
+    } else if (2 == receivedNumber) {
+        basic.showIcon(IconNames.Heart)
+        xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_left, 60, 3)
+    } else if (3 == receivedNumber) {
+        basic.showIcon(IconNames.Asleep)
+        xgo.rotate_angle_continue(xgo.rotate_direction_enum.turn_right, 45, 2)
+    } else if (4 == receivedNumber) {
+        basic.showIcon(IconNames.Confused)
+        xgo.move_xgo(xgo.direction_enum.Backward, 60)
+    } else if (5 == receivedNumber) {
+        basic.showIcon(IconNames.Angry)
+        xgo.execution_action(xgo.action_enum.Sit_down)
+    }
 })
+xgo.init_xgo_serial(SerialPin.P2, SerialPin.P1)
+radio.setGroup(3)
+basic.showIcon(IconNames.Happy)
+music.playSoundEffect(music.builtinSoundEffect(soundExpression.hello), SoundExpressionPlayMode.UntilDone)
